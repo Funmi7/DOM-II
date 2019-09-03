d60aa004cf9dc1a44ec50340bfef0d76de4b3176
Funmi7 <talabifunmilayo@gmail.com>
2019-09-03 12:56:55 +0100

    Added a mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+const headings = document.querySelectorAll("h2");
+
+headings.forEach((heading) =>{
+    heading.addEventListener('mouseover', (event) => {
+        event.target.style.color = '#17A2B8';
+    
+        setTimeout(() => {
+            event.target.style.color = '';
+        }, 500);
+    }, false);
+});
