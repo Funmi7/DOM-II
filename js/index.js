24aa02e75b82f846b2b0425bbf8b02e2d801f6ea
Funmi7 <talabifunmilayo@gmail.com>
2019-09-03 16:13:47 +0100

    Added 5 unique event listeners

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 // Your code goes here
 
-const headings = document.querySelectorAll("h2");
+const headings = document.querySelectorAll('h2');
+const mainHeadings = document.querySelector('h1');
+const navs = document.querySelectorAll('a');
+const subHeadings = document.querySelector('.footer');
+const text = document.querySelector('input');
+const selectedText = document.querySelector('#selectedMessage')
+const buttons = document.querySelectorAll('.btn');
 
-headings.forEach((heading) =>{
+//Event Listeners 1
+headings.forEach((heading) => {
     heading.addEventListener('mouseover', (event) => {
         event.target.style.color = '#17A2B8';
     
@@ -10,4 +17,44 @@ headings.forEach((heading) =>{
             event.target.style.color = '';
         }, 500);
     }, false);
+});
+
+//Event Listeners 2
+document.addEventListener('keydown', () => {
+    mainHeadings.textContent = '';
+
+    setTimeout(() => {
+        mainHeadings.textContent = 'Fun Bus';
+    }, 1000);
+});
+
+//Event Listeners 3
+navs.forEach((nav) => {
+    nav.addEventListener('focus', (event) => {
+        event.target.style.color = '#17A2B8';
+    });
+
+    nav.addEventListener('blur', (event) => {
+        event.target.style.color = '';    
+    });
+});
+
+//Event Listeners 4
+text.addEventListener('select', () => {
+    selectedText.textContent = `The text was selected`;
+
+    setTimeout(() => {
+        selectedText.textContent = ``;
+    }, 1000);
+});
+
+//Event Listeners 5
+buttons.forEach((button) => {
+    button.addEventListener('dblclick', (event) => {
+        event.target.style.backgroundColor = '#FFEBCD';
+
+        setTimeout(() => {
+            event.target.style.backgroundColor = '';
+        }, 1000);
+    }); 
 });
