add0d8025508dc87a2f58001d2c8ab17457d9c84
Funmi7 <talabifunmilayo@gmail.com>
2019-09-04 02:01:26 +0100

    Added nine unique events listeners

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 // Your code goes here
-
+const container = document.querySelector('.home');
 const headings = document.querySelectorAll('h2');
 const mainHeadings = document.querySelector('h1');
 const navs = document.querySelectorAll('a');
 const subHeadings = document.querySelector('.footer');
 const text = document.querySelector('input');
-const selectedText = document.querySelector('#selectedMessage')
+const selectedText = document.querySelector('#selectedMessage');
 const buttons = document.querySelectorAll('.btn');
+const scroll = document.querySelector('.scroll');
+const scrollText = document.createElement('p');
+const resizedCount = document.querySelector('span');
+
 
 //Event Listeners 1
 headings.forEach((heading) => {
@@ -58,3 +62,41 @@ buttons.forEach((button) => {
         }, 1000);
     }); 
 });
+
+
+
+// Event Listener 6
+scroll.addEventListener('scroll', () => {
+    container.appendChild(scrollText);
+    scrollText.textContent = `This div was scrolled`;
+
+    setTimeout(() => {
+        scrollText.textContent = ``;
+    }, 1000);
+});
+
+//Event Listener 7
+function myFunction() {
+alert('page loaded successfully');
+}
+
+//Event listener 8
+window.addEventListener('resize', resizeFunction);
+
+let x = 0;
+function resizeFunction() {
+    let txt = x += 1;
+resizedCount.innerHTML = txt;
+}
+
+//Event listener 9
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.target.style.color = '#FFEBCD';
+        
+        setTimeout(() => {
+            event.target.style.color = '';
+        }, 1000);
+    });
+})
+
